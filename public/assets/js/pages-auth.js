8640dd432ce9bf53e4e482a1aa6490f54086478c
Ederson Arruda Ceccato <edersonceccato@gmail.com>
2024-01-02 12:26:44 -0300

    Finalizado tela de login
    Os usuarios e senhas ficam em um arquivo json e quando o usuario clica em submit, ele verifica se as credenciais existe no json para autorizar o login

--- public/assets/js/pages-auth.js
+++ public/assets/js/pages-auth.js
@@ -1,1 +1,84 @@
-"use strict";const formAuthentication=document.querySelector("#formAuthentication");document.addEventListener("DOMContentLoaded",function(e){var t;formAuthentication&&FormValidation.formValidation(formAuthentication,{fields:{username:{validators:{notEmpty:{message:"Please enter username"},stringLength:{min:6,message:"Username must be more than 6 characters"}}},email:{validators:{notEmpty:{message:"Please enter your email"},emailAddress:{message:"Please enter valid email address"}}},"email-username":{validators:{notEmpty:{message:"Please enter email / username"},stringLength:{min:6,message:"Username must be more than 6 characters"}}},password:{validators:{notEmpty:{message:"Please enter your password"},stringLength:{min:6,message:"Password must be more than 6 characters"}}},"confirm-password":{validators:{notEmpty:{message:"Please confirm password"},identical:{compare:function(){return formAuthentication.querySelector('[name="password"]').value},message:"The password and its confirm are not the same"},stringLength:{min:6,message:"Password must be more than 6 characters"}}},terms:{validators:{notEmpty:{message:"Please agree terms & conditions"}}}},plugins:{trigger:new FormValidation.plugins.Trigger,bootstrap5:new FormValidation.plugins.Bootstrap5({eleValidClass:"",rowSelector:".mb-3"}),submitButton:new FormValidation.plugins.SubmitButton,defaultSubmit:new FormValidation.plugins.DefaultSubmit,autoFocus:new FormValidation.plugins.AutoFocus},init:e=>{e.on("plugins.message.placed",function(e){e.element.parentElement.classList.contains("input-group")&&e.element.parentElement.insertAdjacentElement("afterend",e.messageElement)})}}),(t=document.querySelectorAll(".numeral-mask")).length&&t.forEach(e=>{new Cleave(e,{numeral:!0})})});
+"use strict";
+const formAuthentication = document.querySelector("#formAuthentication");
+document.addEventListener("DOMContentLoaded", function (e) {
+ var t;
+ formAuthentication &&
+  FormValidation.formValidation(formAuthentication, {
+   fields: {
+    username: {
+     validators: {
+      notEmpty: { message: "Por favor, insira seu usuário!" },
+      stringLength: {
+       min: 4,
+       message: "Username must be more than 4 characters",
+      },
+     },
+    },
+    email: {
+     validators: {
+      notEmpty: { message: "Por favor, insira seu usuário!" },
+      emailAddress: { message: "Insira m usuário válido!" },
+     },
+    },
+    "email-username": {
+     validators: {
+      notEmpty: { message: "Por favor, insira seu usuário!" },
+      stringLength: {
+       min: 4,
+       message: "Seu usuário precisa ter mais de 4 caracteres!",
+      },
+     },
+    },
+    password: {
+     validators: {
+      notEmpty: { message: "Por favor, insira sua senha!" },
+      stringLength: {
+       min: 4,
+       message: "Sua senha tem que tem mais de 4 caracteres",
+      },
+     },
+    },
+    "confirm-password": {
+     validators: {
+      notEmpty: { message: "Please confirm password" },
+      identical: {
+       compare: function () {
+        return formAuthentication.querySelector('[name="password"]').value;
+       },
+       message: "The password and its confirm are not the same",
+      },
+      stringLength: {
+       min: 4,
+       message: "Password must be more than 4 characters",
+      },
+     },
+    },
+    terms: {
+     validators: { notEmpty: { message: "Please agree terms & conditions" } },
+    },
+   },
+   plugins: {
+    trigger: new FormValidation.plugins.Trigger(),
+    bootstrap5: new FormValidation.plugins.Bootstrap5({
+     eleValidClass: "",
+     rowSelector: ".mb-3",
+    }),
+    submitButton: new FormValidation.plugins.SubmitButton(),
+    defaultSubmit: new FormValidation.plugins.DefaultSubmit(),
+    autoFocus: new FormValidation.plugins.AutoFocus(),
+   },
+   init: (e) => {
+    e.on("plugins.message.placed", function (e) {
+     e.element.parentElement.classList.contains("input-group") &&
+      e.element.parentElement.insertAdjacentElement(
+       "afterend",
+       e.messageElement
+      );
+    });
+   },
+  }),
+  (t = document.querySelectorAll(".numeral-mask")).length &&
+   t.forEach((e) => {
+    new Cleave(e, { numeral: !0 });
+   });
+});
--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const router = express.Router();
 const { executeQuery } = require('../connect/firebird');
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
 
-
+const usersData = JSON.parse(fs.readFileSync('./server/usersData.json', 'utf-8'))
 
 router.get('/get', async (req, res, next) => {
   const result = await executeQuery('SELECT * FROM tabmovtra');
@@ -10,7 +13,22 @@ router.get('/get', async (req, res, next) => {
      res.status(200).json(result)
 });
 
+router.post('/login', async (req, res, next) => {
+  const { name, password } = req.body;
 
+  // Verifique as credenciais (substitua por um método mais seguro na prática)
+  const user = usersData.find(user => user.username === name && user.password === password);
 
+  if (user) {
+    // Crie um token JWT
+    const token = jwt.sign({ user: name }, process.env.PRIVATE_KEY);
+
+    // Envie o token para o cliente
+    res.json({ token });
+  } else {
+    res.status(401).json({ message: 'Credenciais inválidas' });
+  }
+
+});
 
 module.exports = router;
