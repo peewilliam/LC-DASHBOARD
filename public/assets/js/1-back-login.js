85946359767ff95566b6da57996a1bf7920ee784
Ederson Arruda Ceccato <edersonceccato@gmail.com>
2024-01-10 18:23:14 -0300

    Ajustado para quando tentar passar a rota pela barra sem fazer login

--- public/assets/js/1-back-login.js
+++ public/assets/js/1-back-login.js
@@ -3,6 +3,11 @@ const dataUser = localStorage.getItem('dataUser');
 // Verifique se o usuário está na página de login antes de redirecionar
 if (!dataUser && window.location.pathname !== "/") {
    window.location.href = "/";
-} else if (dataUser && window.location.pathname !== "/dashboard") {
-   window.location.href = "/dashboard";
+} else if (dataUser && window.location.pathname === "/") {
+   // Redirecione para outra página se o usuário estiver logado e na página de login
+   window.location.href = "/pedidos";
+} else if (!dataUser && window.location.pathname === "/pedidos") {
+   // Redirecione para a página de login se o usuário não estiver logado e na página de pedidos
+   window.location.href = "/";
 }
+// Adicione outras condições conforme necessário para mais redirecionamentos
