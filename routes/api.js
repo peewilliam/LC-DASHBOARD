98d6737f4c3b0accb4d589592dccc12ab1072b28
Ederson Arruda Ceccato <edersonceccato@gmail.com>
2024-01-30 14:34:34 -0300

    Alterado e criado rotas para suportar o datatables

--- routes/api.js
+++ routes/api.js
@@ -6,11 +6,131 @@ const fs = require('fs');
 
 const usersData = JSON.parse(fs.readFileSync('./server/usersData.json', 'utf-8'));
 
-router.get('/get', async (req, res, next) => {
-  const result = await executeQuery('SELECT * FROM tabmovtra');
-  
-     res.status(200).json(result)
+router.get('/pedidos', async (req, res, next) => {
+  const draw = req.query.draw;
+
+  // Verificar se a propriedade search existe
+  const search = req.query.search || '';
+
+  let count = 30; // Default 30 cards por status
+  if (search) {
+    count = 9999999999; // Se tiver pesquisar, trás qualquer coisa do banco
+  }
+
+  const result = await executeQuery(`
+    SELECT
+      PED.NOMOVTRA,
+      CLI.NOMCLI AS CLIENTE,
+      COALESCE(PED.CONTAINER, 'CARGA SOLTA') AS CONTAINER,
+      CASE PED.NOSTATUSFRE
+        WHEN 1 THEN 'CONCLUIDO'
+        WHEN 2 THEN 'CANCELADO'
+        WHEN null THEN 'SEM_STATUS'
+        WHEN 3 THEN 'DOCUMENTOS_ENTREGUES'
+        WHEN 4 THEN 'DEVOLUÇÃO_DE_VAZIO'
+        WHEN 5 THEN 'CNTR_VAZIO_AGENDADO'
+      END AS STATUS,
+      FLW.DATA AS DATA_FOLLOW
+    FROM
+      TABMOVTRA PED
+    LEFT OUTER JOIN
+      TABCLI CLI ON CLI.NOCLI = PED.NOCLI
+    LEFT OUTER JOIN
+      TABSTATUSFRE STF ON STF.NOSTATUSFRE = PED.NOSTATUSFRE
+    LEFT OUTER JOIN (
+      SELECT
+        FLW.NOMOVTRA,
+        MAX(FLW.DATA) AS DATA
+      FROM
+        TABFALLOW FLW
+      GROUP BY
+        FLW.NOMOVTRA
+    ) FLW ON FLW.NOMOVTRA = PED.NOMOVTRA
+    WHERE
+      (
+        SELECT COUNT(*)
+        FROM TABMOVTRA
+        WHERE NOSTATUSFRE = PED.NOSTATUSFRE
+          AND NOMOVTRA >= PED.NOMOVTRA
+      ) <= ${count}
+    ${search ? `AND (PED.NOMOVTRA LIKE '%${search}%' OR CLI.NOMCLI LIKE '%${search}%' OR PED.CONTAINER LIKE '%${search}%')` : ''}
+    ORDER BY
+      PED.NOMOVTRA DESC
+  `);
+
+  res.status(200).json({
+    draw: draw,
+    recordsTotal: result.length, // O total de registros no conjunto de dados (sem filtros)
+    recordsFiltered: result.length, // O total de registros após a aplicação de filtros
+    data: result,
+  });
 });
+
+router.get('/pedidos-modal/:nomovtra', async (req, res, next) => {
+  const { nomovtra } = req.params;
+
+  try {
+    const result = await executeQuery(`
+      SELECT
+        PED.NOMOVTRA,
+        CLI.NOMCLI AS CLIENTE,
+
+        COL.NOMCLI AS COLETA,
+        CCOL.NOMCID AS CID_COLETA,
+        CCOL.UFCID AS UF_COLETA,
+
+        DEST.NOMCLI AS DESTINATARIO,
+        CDEST.NOMCID AS CID_DESTINATARIO,
+        CDEST.UFCID AS UF_DESTINATARIO,
+
+        DISTR.NOMCLI AS ENTREGA,
+        CDISTR.NOMCID AS CID_ENTREGA,
+        CDISTR.UFCID AS UF_ENTREGA,
+
+        REM.NOMCLI AS REMETENTE,
+        CREM.NOMCID AS CID_REMETENTE,
+        CREM.UFCID AS UF_REMETENTE,
+        
+
+        FLW.DATA AS DATA_FOLLOW,
+        FLW.STATUS AS STATUS_FOLLOW,
+        CAST(FLW.OBS AS VARCHAR(8191) CHARACTER SET ISO8859_1) AS OBS_FOLLOW,
+
+        PED.TOTALFRETE,
+        PED.VLRMOT
+
+      FROM
+        TABMOVTRA PED
+      LEFT OUTER JOIN
+        TABCLI CLI ON CLI.NOCLI = PED.NOCLI
+      LEFT OUTER JOIN
+        TABCLI COL ON COL.NOCLI = PED.NOTERM_COL
+      LEFT OUTER JOIN
+        TABCLI DEST ON DEST.NOCLI = PED.NOCLI_DEST
+      LEFT OUTER JOIN
+        TABCLI DISTR ON DISTR.NOCLI = PED.NOTERM_DEST
+      LEFT OUTER JOIN
+        TABCLI REM ON REM.NOCLI = PED.NOCLI_REM
+      LEFT OUTER JOIN
+        TABCID CCOL ON CCOL.NOCID = COL.NOCID
+      LEFT OUTER JOIN
+        TABCID CDEST ON CDEST.NOCID = DEST.NOCID
+      LEFT OUTER JOIN
+        TABCID CDISTR ON CDISTR.NOCID = DISTR.NOCID
+      LEFT OUTER JOIN
+        TABCID CREM ON CREM.NOCID = REM.NOCID
+      LEFT OUTER JOIN
+        TABFALLOW FLW ON FLW.NOMOVTRA = PED.NOMOVTRA
+      WHERE
+        PED.NOMOVTRA = ${nomovtra}
+    `);
+
+    res.json(result);
+  } catch (error) {
+    console.error('Erro na consulta SQL:', error);
+    res.status(500).json({ error: 'Erro na consulta SQL' });
+  }
+})
 
 router.post('/login', async (req, res, next) => {
   const { name, password } = req.body;
@@ -33,13 +153,27 @@ router.get('/modulos', (req, res) => {
   res.json(modulos);
 })
 
-router.get('/usuarios', (req, res) => {
-  // Pega os modulos disponiveis no usuario superuser, porque nele tem todos os modulos
-  const usuarios = usersData
-    .filter(user => user.id !== 1) // Não puxa o superuser
-    // .map(user => user.username) // Vai apresentar todos os demais
-  res.json(usuarios);
-})
+router.get('/usuarios', async (req, res) => {
+  const draw = req.query.draw;
+
+  // Verificar se a propriedade search existe
+  const search = req.query.search || '';
+
+  const filtrar_usuarios = usersData.filter(user => {
+    // Remove o usuário superuser
+    return (
+      user.username.toLowerCase().includes(search.toLowerCase()) ||
+      user.module.some(module => module.toLowerCase().includes(search.toLowerCase()))
+    ) && user.id !== 1;
+  });
+
+  res.json({
+    draw: draw,
+    recordsTotal: usersData.length, // O total de registros no conjunto de dados (sem filtros)
+    recordsFiltered: filtrar_usuarios.length, // O total de registros após a aplicação de filtros
+    data: filtrar_usuarios,
+  });
+});
 
 router.post('/atualizar-usuario/:id', (req, res) => {
   const { id } = req.params; // Obtenha o ID da URL
